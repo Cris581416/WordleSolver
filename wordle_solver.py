32471f720a173e8680da288bd183efb59de3d419
Cristobal Velazquez <cvelazquez581416@gmail.com>
2022-03-17 14:36:16 -0700

    Update

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,28 +1,27 @@
 import PySimpleGUI as sg
 
 
-sg.theme('DarkBlue')   # Add a touch of color
+sg.theme('DarkPurple3')  # Add a touch of color
 
-def space(spaces):
-    return sg.Text(text="", size=(spaces, 1))
+# Messages
+checkbox_message = """If checked, will assume that the character in the input box (can only be 1 character) must be in this slot.
+If unchecked, will assume that the characters in the input box are required but can't be in this slot"""
 
 # All the stuff inside your window.
 layout = [  [sg.Text(text="Enter Bad Letters Here:", size=(30, 1))],
             [sg.Input(size=(30, 1), key='used')],
             [sg.Text(text="Enter Good Letters Here:", size=(30, 1))],
             [sg.Input(size=(30, 1), key="required")],
-            [sg.Checkbox("Include", key='include0'), sg.Input(size=(30, 1), key='slot0')],
-            [sg.Checkbox("Include", key='include1'), sg.Input(size=(30, 1), key='slot1')],
-            [sg.Checkbox("Include", key='include2'), sg.Input(size=(30, 1), key='slot2')],
-            [sg.Checkbox("Include", key='include3'), sg.Input(size=(30, 1), key='slot3')],
-            [sg.Checkbox("Include", key='include4'), sg.Input(size=(30, 1), key='slot4')],
+            [sg.Checkbox("Include", key='include0', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot0')],
+            [sg.Checkbox("Include", key='include1', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot1')],
+            [sg.Checkbox("Include", key='include2', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot2')],
+            [sg.Checkbox("Include", key='include3', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot3')],
+            [sg.Checkbox("Include", key='include4', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot4')],
             [sg.Multiline(default_text="No results yet!", size=(39, 4), key='results', disabled=True)],
             [sg.Button('Calculate'), sg.Exit()]]
 
 # Create the Window
 window = sg.Window('Wordle Solver', layout)
-#window.Maximize()
-
 
 # LOAD WORDS
 with open("words.txt") as word_file:
@@ -37,9 +36,6 @@ def is_english_word(word):
 def selection(chars, choose, letters):
     """'chars' provides letters to exempt from selection unless choose == 1, in which case 'chars' should only have 1 character in it: the one desired for selection"""
 
-    print("chars =", chars)
-    print("choose =", choose)
-
     if choose == "1":
         return set(letters) - set(x for x in letters if x is not chars)
     else:
@@ -53,12 +49,6 @@ def get_possible_words(chars_a, chars_b, chars_c, chars_d, chars_e, possible_let
     set_c = selection(chars_c[:-1], chars_c[-1], possible_letters)
     set_d = selection(chars_d[:-1], chars_d[-1], possible_letters)
     set_e = selection(chars_e[:-1], chars_e[-1], possible_letters)
-
-    print("set_a =", set_a)
-    print("set_b =", set_b)
-    print("set_c =", set_c)
-    print("set_d =", set_d)
-    print("set_e =", set_e)
 
     required = list(required_letters)
 
@@ -99,25 +89,13 @@ while True:
     chars_d = values["slot3"].strip() + str(int(values["include3"] == True))
     chars_e = values["slot4"].strip() + str(int(values["include4"] == True))
 
-    print("chars_a =", chars_a)
-    print("chars_b =", chars_b)
-    print("chars_c =", chars_c)
-    print("chars_d =", chars_d)
-    print("chars_e =", chars_e)
-
     used = values["used"].strip()
 
-    print("used =", used)
-
     letters = [i for i in all_letters if i not in list(used)]
-
-    print("letters =", letters)
 
     required_letters = values["required"].strip()
 
     words = get_possible_words(chars_a, chars_b, chars_c, chars_d, chars_e, letters, required_letters)
-
-    print(words)
 
     window['results'].update(words)
 
