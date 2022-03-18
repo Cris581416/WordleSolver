84d3dc46df1b066c6f9d320ab8ad68a6efe31dc4
Cristobal Velazquez <cvelazquez581416@gmail.com>
2022-03-17 19:38:25 -0700

    Add theme selection

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,27 +1,33 @@
 import PySimpleGUI as sg
 
 
-sg.theme('DarkPurple3')  # Add a touch of color
-
 # Messages
 checkbox_message = """If checked, will assume that the character in the input box (can only be 1 character) must be in this slot.
 If unchecked, will assume that the characters in the input box are required but can't be in this slot"""
 
+def make_window(theme=None):
+    if theme:
+        sg.theme(theme)
+    # -----  Layout & Window Create  -----
+    layout = [  [sg.Text(text="Enter Bad Letters Here:", size=(30, 1))],
+                [sg.Input(size=(30, 1), key='used')],
+                [sg.Text(text="Enter Good Letters Here:", size=(30, 1))],
+                [sg.Input(size=(30, 1), key="required")],
+                [sg.Checkbox("Include", key='include0', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot0')],
+                [sg.Checkbox("Include", key='include1', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot1')],
+                [sg.Checkbox("Include", key='include2', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot2')],
+                [sg.Checkbox("Include", key='include3', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot3')],
+                [sg.Checkbox("Include", key='include4', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot4')],
+                [sg.Multiline(default_text="No results yet!", size=(39, 4), key='results', disabled=True)],
+                [sg.Button('Calculate'), sg.Exit(), sg.Button('Change Theme')]]
+
+    return sg.Window('Wordle Solver', layout)
+
 # All the stuff inside your window.
-layout = [  [sg.Text(text="Enter Bad Letters Here:", size=(30, 1))],
-            [sg.Input(size=(30, 1), key='used')],
-            [sg.Text(text="Enter Good Letters Here:", size=(30, 1))],
-            [sg.Input(size=(30, 1), key="required")],
-            [sg.Checkbox("Include", key='include0', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot0')],
-            [sg.Checkbox("Include", key='include1', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot1')],
-            [sg.Checkbox("Include", key='include2', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot2')],
-            [sg.Checkbox("Include", key='include3', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot3')],
-            [sg.Checkbox("Include", key='include4', tooltip=checkbox_message), sg.Input(size=(30, 1), key='slot4')],
-            [sg.Multiline(default_text="No results yet!", size=(39, 4), key='results', disabled=True)],
-            [sg.Button('Calculate'), sg.Exit()]]
+
 
 # Create the Window
-window = sg.Window('Wordle Solver', layout)
+window = make_window('Python')
 
 # LOAD WORDS
 with open("words.txt") as word_file:
@@ -52,8 +58,6 @@ def get_possible_words(chars_a, chars_b, chars_c, chars_d, chars_e, possible_let
 
     required = list(required_letters)
 
-    print("Required =", required)
-
     words = ""
 
     for a in set_a:
@@ -80,23 +84,53 @@ while True:
 
     if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
         break
+    
+    elif event == "Calculate":
 
-    all_letters = [i for i in "abcdefghijklmnopqrstuvwxyz"]
+        all_letters = [i for i in "abcdefghijklmnopqrstuvwxyz"]
 
-    chars_a = values["slot0"].strip() + str(int(values["include0"] == True))
-    chars_b = values["slot1"].strip() + str(int(values["include1"] == True))
-    chars_c = values["slot2"].strip() + str(int(values["include2"] == True))
-    chars_d = values["slot3"].strip() + str(int(values["include3"] == True))
-    chars_e = values["slot4"].strip() + str(int(values["include4"] == True))
+        chars_a = values["slot0"].strip() + str(int(values["include0"] == True))
+        chars_b = values["slot1"].strip() + str(int(values["include1"] == True))
+        chars_c = values["slot2"].strip() + str(int(values["include2"] == True))
+        chars_d = values["slot3"].strip() + str(int(values["include3"] == True))
+        chars_e = values["slot4"].strip() + str(int(values["include4"] == True))
 
-    used = values["used"].strip()
+        used = values["used"].strip()
 
-    letters = [i for i in all_letters if i not in list(used)]
+        letters = [i for i in all_letters if i not in list(used)]
 
-    required_letters = values["required"].strip()
+        required_letters = values["required"].strip()
 
-    words = get_possible_words(chars_a, chars_b, chars_c, chars_d, chars_e, letters, required_letters)
+        words = get_possible_words(chars_a, chars_b, chars_c, chars_d, chars_e, letters, required_letters)
 
-    window['results'].update(words)
+        window['results'].update(words)
+
+    elif event == "Change Theme":
+        event, values = sg.Window('Choose Theme', [[sg.Combo(sg.theme_list(), readonly=True, k='themes'), sg.OK(), sg.Cancel()]]).read(close=True)
+
+        if event == 'OK':
+            # ---- Switch to your new theme! ---- IMPORTANT PART OF THE PROGRA<
+            window.close()
+            window = make_window(values['themes'])
 
 window.close()
+
+
+# ------------------- Main Program and Event Loop -------------------
+def main():
+    window = make_window()
+
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Exit':
+            break
+        if event == 'Change Theme':      # Theme button clicked, so get new theme and restart window
+            event, values = sg.Window('Choose Theme',
+                                      [[sg.Combo(sg.theme_list(), readonly=True, k='-THEME LIST-'), sg.OK(), sg.Cancel()]]
+                                      ).read(close=True)
+            if event == 'OK':
+                # ---- Switch to your new theme! ---- IMPORTANT PART OF THE PROGRA<
+                window.close()
+                window = make_window(values['-THEME LIST-'])
+
+    window.close()
